6d51bc45d233fe9d52964b998cc75b52cc6efd7c
Alberto <albertoflj@gmail.com>
2023-01-28 04:00:05 +0200

    Added header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,7 @@
 import chatIcon from "../../assets/icons/chat-dots.svg";
+import postIcon from "../../assets/icons/plus-square.svg";
+import homeIcon from "../../assets/icons/house-door-fill.svg";
+
 import "../../main-styles/main.scss";
 import "./header.scss";
 const Header = () => {
@@ -10,6 +13,20 @@ const Header = () => {
         <button id="messages-icon" className="mobile">
           <img src={chatIcon} alt="chatIcon" />
         </button>
+        <div className="header-right-buttons desktop">
+          <button className="post-button">
+            <img src={postIcon} alt="post-icon" />
+          </button>
+          <button className="home-button">
+            <img src={homeIcon} alt="home-icon" />
+          </button>
+          <button className="messages-button">
+            <img src={chatIcon} alt="chatIcon" />
+          </button>
+          <button className="profile-button">
+            <div className="profile-photo-placeholder"></div>
+          </button>
+        </div>
       </div>
     </header>
   );
